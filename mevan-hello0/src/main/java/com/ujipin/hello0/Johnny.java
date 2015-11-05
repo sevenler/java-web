0305822aa626dc28086a5efbea2f2051e40f4afa
Johnnyxyz <johnnyxyz@163.com>
2015-11-06 00:13:37 +0800

    add mevan link

---
+++ mevan-hello0/src/main/java/com/ujipin/hello0/Johnny.java
@@ -0,0 +1,12 @@
+package com.ujipin.hello0;
+
+import com.ujipin.hello.User;
+
+/**
+ * Created by Johnny on 15/11/5.
+ */
+public class Johnny {
+    public void sayHello(){
+          new User().sayHello();
+    }
+}
---
+++ mevan-hello0/src/test/java/TestCase.java
@@ -0,0 +1,14 @@
+import com.ujipin.hello0.Johnny;
+import org.junit.Test;
+
+/**
+ * Created by Johnny on 15/11/5.
+ */
+public class TestCase {
+
+    @Test
+    public void testHello(){
+        Johnny user = new Johnny();
+        user.sayHello();
+    }
+}
