593fb423f083b389e9d5619313effcc61a656e4f
Johnnyxyz <johnnyxyz@163.com>
2015-11-06 11:26:17 +0800

    add web-app demo code

---
+++ web-app1/src/main/java/com/ujipin/webapp1/Johnny.java
@@ -0,0 +1,10 @@
+package com.ujipin.webapp1;
+
+/**
+ * Created by Johnny on 15/11/5.
+ */
+public class Johnny {
+    public void sayHello(){
+        System.out.println("hello , this is johnny");
+    }
+}
---
+++ web-app1/src/test/java/TestCase.java
@@ -0,0 +1,13 @@
+import com.ujipin.webapp1.Johnny;
+import org.junit.Test;
+
+/**
+ * Created by Johnny on 15/11/6.
+ */
+public class TestCase {
+    @Test
+    public void testHello(){
+        Johnny user = new Johnny();
+        user.sayHello();
+    }
+}
